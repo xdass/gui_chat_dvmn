c4ea0265aba61803c5424bf15debd0b6221391d0
dass <420600base>
2020-01-28 13:17:52 +0500

    Добавлен графический интерфейс регистрации пользователя

--- registration.py
+++ registration.py
@@ -1,22 +1,81 @@
+import os
+from asyncio import Queue
+import asyncio
+import json
 from tkinter import *
 from tkinter import messagebox
+from main import connect
+from util import create_handy_nursery
+from gui import TkAppClosed, update_tk
+import aiofiles
+from dotenv import load_dotenv
 
 
-def register(input):
-    if len(input) > 1:
-        print(input)
-        messagebox.showinfo("Успешно!", "Пользователь создан, токен сохранен в файл")
+async def process(queue):
+    host = os.getenv("HOST")
+    write_port = os.getenv("WRITE_PORT")
+    reader, writer = await connect(host, write_port)
+    await reader.readline()
+    username = await queue.get()
+    writer.write("\n".encode())
+    await writer.drain()
+    await reader.readline()
+    writer.write(f"{username}\n".encode())
+    await writer.drain()
+    register_response = await reader.readline()
+    decoded_json = json.loads(register_response.decode())
+    await save_credentials("credentials.txt", decoded_json)
+
+
+def get_name(input_field, queue):
+    value = input_field.get(1.0, END)
+    if len(value) > 1:
+        queue.put_nowait(value)
+        messagebox.showinfo("Успешно!", f"Пользователь {value} создан, токен сохранен в файл")
+        input_field.delete(1.0, END)
     else:
         messagebox.showerror("Ошибка!", "Введите имя пользователя!")
 
 
-root = Tk()
-root.title("Регистрация пользователя(minechat)")
-label = Label(text="Введите имя пользователя")
-btn = Button(text="Зарегистрироваться", width=15, height=2, padx=10)
-edit = Text(width=30, height=1)
-label.pack(side=LEFT)
-edit.pack(side=LEFT)
-btn.pack(side=RIGHT)
-btn["command"] = lambda: register(edit.get(1.0, END))
-root.mainloop()
+async def save_credentials(filepath, creds):
+    async with aiofiles.open(filepath, "a", encoding="utf8") as fh:
+        await fh.write(f"{creds['nickname']}=={creds['account_hash']}\n")
+
+
+async def draw(sending_queue):
+    root = Tk()
+
+    root.title('Регистрация пользователя(minechat)')
+
+    root_frame = Frame()
+    root_frame.pack(fill="both", expand=True, padx=20, pady=20)
+
+    input_frame = Frame(root_frame)
+    input_frame.pack(side="bottom", fill=X)
+
+    input_field = Text(input_frame, width=30, height=1)
+    input_field.pack(side="left", fill=X, expand=True)
+
+    send_button = Button(input_frame, height=1)
+    send_button["text"] = "Зарегистрироваться"
+    send_button["command"] = lambda: get_name(input_field, sending_queue)
+    send_button.pack(side="left")
+
+    await asyncio.gather(
+        update_tk(root_frame),
+        process(sending_queue)
+    )
+
+
+async def main():
+    load_dotenv()
+    name_queue = Queue()
+
+    try:
+        async with create_handy_nursery() as nursery:
+            nursery.start_soon(draw(name_queue))
+    except TkAppClosed:
+        sys.exit()
+
+if __name__ == '__main__':
+    asyncio.run(main())
